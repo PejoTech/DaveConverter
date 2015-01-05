fa6eb70c890a1fb487c85207e485a0111b1cc3af
Peter Weber <peter@appswithlove.com>
2015-01-05 11:41:23 +0100

    Init first draw

---
+++ DaveConverter.CommandLine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DaveConverter.Common;
+
+namespace DaveConverter.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pfad zum .itf angeben:");
+
+            var fileText = ReadFile.LoadStringFromFile(Console.ReadLine());
+
+            Console.WriteLine("TABL Abschnitte einzeln augeben? (j/n)");
+
+            switch (Console.ReadLine())
+            {
+                case "j":
+                {
+                    ReadFile.WriteTextFromFileByLine(fileText);
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine(fileText);
+                    Console.ReadLine();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ DaveConverter.Common/ReadFile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaveConverter.Common
+{
+    public class ReadFile
+    {
+        public static void WriteTextFromFileByLine(string file)
+        {
+            using (var reader = new StringReader(file))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("TABL"))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Sektion {0} wird angezeigt", line);
+                        Console.ReadLine();
+                    }
+
+                    Console.WriteLine(line);
+                }
+            }
+
+            // Suspend the screen.
+            Console.ReadLine();
+        }
+
+        public static String LoadStringFromFile(string path)
+        {
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read: {0}", path);
+                Console.WriteLine(e.Message);
+            }
+
+            return "";
+        }
+    }
+}
